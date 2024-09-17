fcf47705f99432066337417ac4d511a73db01a01
Liam-So <Liam-So@dal.ca>
2024-09-17 17:12:31 -0400

    Initial Flask server setup

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def hello():
+  return 'Welcome to FinTrack 💸'
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)

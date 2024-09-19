efd90f4b986cceeea69e512b51dc828a20fbbed6
Liam-So <Liam-So@dal.ca>
2024-09-19 11:05:50 -0400

    setup frontend

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,11 +2,27 @@ from flask import Blueprint, request, jsonify
 from app.services.pdf_extractor import PDFExtractor, Institution
 from app.services.transaction_extractor import TransactionExtractor
 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.Util.Padding import unpad
+
 bp = Blueprint('api', __name__)
+
+# Generate RSA key pair (do this securely and store the private key safely)
+key = RSA.generate(2048)
+private_key = key.export_key()
+public_key = key.publickey().export_key()
+
 
 @bp.route('/', methods=['GET'])
 def hello():
     return 'Welcome to FinTrack 💸'
+
+
+@bp.route('/get-public-key', methods=['GET'])
+def get_public_key():
+    return public_key.decode(), 200
+
 
 @bp.route('/extract', methods=['POST'])
 def extract():

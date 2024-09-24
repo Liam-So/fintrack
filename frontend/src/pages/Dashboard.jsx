2fe781b9ef081de1bacdc539d95730d088d61a0c
Liam-So <Liam.So@dal.ca>
2024-09-24 15:43:00 -0400

    refactor axios calls

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { api } from '../axios';
 
 const Dashboard = () => {
+
+  useEffect(() => {
+    api.get('/categories/percentages')
+      .then(response => console.log(response.data))
+      .catch(error => console.error('Error fetching public key:', error));
+  }, []);
+
   return (
     <div>Dashboard</div>
   )

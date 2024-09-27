d4c15593bd82c703ae88a5a869dbf5abb910fa8b
Liam-So <Liam.So@dal.ca>
2024-09-27 15:58:40 -0400

    Add basic auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+const domain = import.meta.env.VITE_AUTH_DOMAIN
+const clientId = import.meta.env.VITE_CLIENT_ID
+
+root.render(
+<Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>,
+);

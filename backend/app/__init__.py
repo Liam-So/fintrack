f7eba96360f5ef7f4a97c6cfe9bea634a550f168
Liam-So <Liam-So@dal.ca>
2024-10-01 14:05:56 -0400

    starter code for db setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,18 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://liam.so:password@localhost:5432/fintrack')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
     CORS(app)
 
     from app.api import routes

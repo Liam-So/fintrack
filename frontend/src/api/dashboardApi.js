b39ba88d50a4ea930d8a1737878eb03310768dd5
Liam-So <Liam-So@dal.ca>
2024-10-02 09:47:13 -0400

    refactor dashboard

---
+++ frontend/src/api/dashboardApi.js
@@ -0,0 +1,20 @@
+import { api } from "../axios";
+
+export const fetchUserData = (user) => (api.get(`user/${user.nickname}`));
+
+export const fetchTransactions = () => (api.get("/transactions"));
+
+export const fetchCategoryPercentages = () => (api.get("/categories/percentages"));
+
+export const postCalculateCategoryPercentages = (transactions) => (
+  api.post("/trial/categories/percentages", {
+    transactions: transactions
+  })
+);
+
+export const postNewUser = (user) => (
+  api.post("/user", {
+    username: user.nickname,
+    email: user.email
+  })
+);

da480035c73cdb0123d50e361d57881622ea2b51
Liam-So <Liam-So@dal.ca>
2024-10-07 11:53:22 -0400

    decouple dashboard component

--- frontend/src/pages/TrialDashboard.jsx
+++ frontend/src/pages/TrialDashboard.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import Dashboard from './Dashboard'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+import { fetchTrialTransactions, postCalculateCategoryPercentages } from '../api/dashboardApi';
+import DashboardUI from '../components/DashboardUI';
 
 const TrialDashboard = () => {
+  const location = useLocation();
+  const { state } = location;
+
+  const [monthlyIncome, setMonthlyIncome] = useState(0);
+  const [transactions, setTransactions] = useState([]);
+  const [categoryPercentages, setCategoryPercentages] = useState([]);
+  const [amountSpent, setAmountSpent] = useState(0);
+
+  // Add one to month because it is zero indexed
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        setMonthlyIncome(state.income);
+        const { data: newTransactions } = await fetchTrialTransactions(state.transactions, month);
+        setTransactions(newTransactions.transactions);
+
+        const { data: postCategoryPercentages } = await postCalculateCategoryPercentages(newTransactions.transactions);
+        setCategoryPercentages(postCategoryPercentages);
+
+        const totalAmount = Object.values(postCategoryPercentages).reduce((acc, curr) => acc + curr, 0).toFixed(2);
+        setAmountSpent(totalAmount);
+      } catch (err) {
+        console.error('Error fetching transactions:', err);
+      }
+    }
+    
+      fetchDashboardData();
+  }, [state, month]);
+  
+
   return (
-    <Dashboard isTrialDashboard />
+    <DashboardUI 
+      transactions={transactions}
+      categoryPercentages={categoryPercentages}
+      amountSpent={amountSpent}
+      monthlyRevenue={monthlyIncome}
+      month={month}
+      setMonth={setMonth}
+    />
   )
 }
 

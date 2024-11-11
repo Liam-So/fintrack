5f248b5101bae53f32f96ebd292e8ec3f00f7640
Liam-So <Liam-So@dal.ca>
2024-11-11 18:48:38 -0500

    refactor donut

--- frontend/src/components/DonutChart.jsx
+++ frontend/src/components/DonutChart.jsx
@@ -35,38 +35,45 @@ const DonutChart = ({ categoryPercentages }) => {
 
   return (
     <>
-    {categoryPercentages ? (
-      <Doughnut
-        data={doughnutData(categoryPercentages)}
-        options={{
-          plugins: {
-            legend: { position: "bottom" },
-            datalabels: {
-              display: true,
-              color: "#fff",
-              formatter: (_, context) => {
-                const categoryValues = Object.values(categoryPercentages);
-                const total = categoryValues.reduce((acc, curr) => acc + curr, 0);
-                const staticLabels = categoryValues.map(
-                  (value) => `${Math.round((value / total) * 100)}%`,
-                );
-                return staticLabels[context.dataIndex];
+      {categoryPercentages && (
+        <Doughnut
+          data={doughnutData(categoryPercentages)}
+          options={{
+            plugins: {
+              legend: {
+                position: "bottom",
+                labels: {
+                  padding: 20,
+                  usePointStyle: true,
+                  pointStyle: 'circle',
+                  font: {
+                    size: 12,
+                    weight: 500
+                  }
+                }
               },
-              font: {
-                size: 13,
-                weight: 500,
+              datalabels: {
+                display: true,
+                color: "#fff",
+                formatter: (_, context) => {
+                  const categoryValues = Object.values(categoryPercentages);
+                  const total = categoryValues.reduce((acc, curr) => acc + curr, 0);
+                  const staticLabels = categoryValues.map(
+                    (value) => `${Math.round((value / total) * 100)}%`,
+                  );
+                  return staticLabels[context.dataIndex];
+                },
+                font: {
+                  size: 13,
+                  weight: 500,
+                },
               },
             },
-          },
-          cutout: "70%",
-          maintainAspectRatio: false,
-        }}
-      />
-    ) : (
-      <p className="text-md">
-      No data 💀
-      </p>
-    )}
+            cutout: "70%",
+            maintainAspectRatio: false,
+          }}
+        />
+      )}
     </>
   );
 };

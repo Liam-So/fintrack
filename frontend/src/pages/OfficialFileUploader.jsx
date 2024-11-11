31536fb70b5cc3599d0a1641c4ac419f7c616f73
Liam-So <Liam-So@dal.ca>
2024-11-10 22:28:12 -0500

    refactor file uploader for official route

---
+++ frontend/src/pages/OfficialFileUploader.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import { useUser } from '../context/UserContext';
+import TransactionTable from '../components/TransactionTable';
+import { postTransactions } from '../api/dashboardApi';
+import { useNavigate, useParams } from 'react-router-dom';
+import FileUploader from '../components/FileUploader';
+
+const OfficialFileUploader = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const { user } = useUser();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const categories = user.categories;
+
+  const handleSubmit = async () => {
+    await postTransactions(transactions, id);
+    navigate('/dashboard');
+  }
+
+  const handleDelete = (id) => {
+    const filteredTransactions = transactions.filter(t => t.id !== id);
+    setTransactions(filteredTransactions);
+  }
+
+  const handleSave = (id, transaction) => {
+    const updatedTransactions = transactions.map(t => t.id === id ? { ...t, ...transaction } : t).sort((a, b) => new Date(b.date) - new Date(a.date));
+    setTransactions(updatedTransactions);
+  }
+
+  const handleAdd = (newTransaction) => {
+    const newTransactions = [...transactions, newTransaction].sort((a, b) => new Date(b.date) - new Date(a.date));
+    setTransactions(newTransactions);
+  }
+
+  return (
+    <>
+      {transactions.length > 0 ? (
+        <TransactionTable
+          postedTransactions={transactions}
+          handleSubmit={handleSubmit}
+          handleDeleteAction={handleDelete}
+          handleSaveAction={handleSave}
+          handleAddAction={handleAdd}
+        />
+      ) : (
+        <FileUploader
+          categories={categories}
+          setTransactions={setTransactions}
+        />
+      )}
+    </>
+  )
+}
+
+export default OfficialFileUploader

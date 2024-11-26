a55d58d60f5a1b0513c4972858f06cb9a97a90e9
Liam-So <Liam.So@dal.ca>
2024-11-25 23:16:55 -0500

    create feature flags in the backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,24 +4,42 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import get_config
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://liam.so:password@localhost:5432/fintrack')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app(config_class=None):
+  app = Flask(__name__)
 
+  # load config
+  if config_class is None:
+    config_class = get_config()
+  
+  app.config.from_object(config_class)
+
+  if app.config.get('SQLALCHEMY_DATABASE_URI'):
     db.init_app(app)
     Migrate(app, db)
 
-    CORS(app)
+  CORS(app)
 
-    from app.api.routes import user_routes, transaction_routes, extraction_routes, trial_routes
-
-    app.register_blueprint(user_routes.user_bp, url_prefix='/api/user')
-    app.register_blueprint(transaction_routes.transaction_bp, url_prefix='/api/transactions')
-    app.register_blueprint(extraction_routes.extraction_bp, url_prefix='/api/extraction')
+  routes_config = app.config.get('ROUTES_CONFIG', {})
+  print(routes_config)
+    
+  if routes_config.get('enable_trial_routes'):
+    from app.api.routes import trial_routes
     app.register_blueprint(trial_routes.trial_bp, url_prefix='/api/trial')
+  
+  if routes_config.get('enable_user_routes'):
+    from app.api.routes import user_routes
+    app.register_blueprint(user_routes.user_bp, url_prefix='/api/user')
+  
+  if routes_config.get('enable_transaction_routes'):
+    from app.api.routes import transaction_routes
+    app.register_blueprint(transaction_routes.transaction_bp, url_prefix='/api/transactions')
+  
+  if routes_config.get('enable_extraction_routes'):
+    from app.api.routes import extraction_routes
+    app.register_blueprint(extraction_routes.extraction_bp, url_prefix='/api/extraction')
 
-    return app
+  return app

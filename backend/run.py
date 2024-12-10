af1c904585b8c1a593e5a14c8abbc5a3e57d8108
Liam-So <Liam.So@dal.ca>
2024-12-10 15:27:28 -0500

    run for production build

--- backend/run.py
+++ backend/run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-  # app.run(port=8000, debug=True, ssl_context='adhoc')
-  app.run(port=8000, debug=True)
+  app.run()
+  # app.run(port=8000, debug=True)

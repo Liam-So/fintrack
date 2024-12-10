f9454c14937e5c5946f5a9745ca75d0031b923a3
Liam-So <Liam.So@dal.ca>
2024-12-10 16:02:18 -0500

    debug url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.DEMO_MODE ? import.meta.env.VITE_DEMO_BACKEND_URL : import.meta.env.VITE_DEV_BACKEND_URL;
+console.log('baseURL:', baseURL);
+
 export const api = axios.create({
-  baseURL: import.meta.env.DEMO_MODE ? import.meta.env.VITE_DEMO_BACKEND_URL : import.meta.env.VITE_DEV_BACKEND_URL,
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json'
   }
